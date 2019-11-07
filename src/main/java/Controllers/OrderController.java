c906fbdd23e3b12df4ac49fb2e3ba113215ab1bc
88710 <88710@farnborough.ac.uk>
2019-11-07 14:27:43 +0000

    test

--- src/main/java/Controllers/OrderController.java
+++ src/main/java/Controllers/OrderController.java
@@ -5,15 +5,25 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class OrderController {
-    public static void insertOrder(int ReferenceNo, String DateOrder, double OrderPrice, boolean PaymentSuccessful, String PaymentType, String DeliveryOrCollection, String CustomerName) {
+    @POST
+    @Path("new")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String insertOrder(@FormDataParam("ReferenceNo") Integer ReferenceNo, @FormDataParam("DateOrder") String DateOrder, @FormDataParam("OrderPrice") double OrderPrice, @FormDataParam("PaymentSuccessful") boolean PaymentSuccessful, @FormDataParam("PaymentType") String PaymentType, @FormDataParam("DeliveryOrCollection") String DeliveryOrCollection, @FormDataParam("CustomerName") String CustomerName) {
         try {
+            if (ReferenceNo == null || DateOrder == null || PaymentType == null || DeliveryOrCollection == null || CustomerName == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+            System.out.println("thing/new ReferenceNo=" + ReferenceNo);
             PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Orders(ReferenceNO, DateOrder, OrderPrice, PaymentSuccessful, PaymentType, DeliveryOrCollection, CustomerName) Values (?,?,?,?,?,?,?)");
             ps.setInt(1, ReferenceNo);
             ps.setString(2, DateOrder);
@@ -24,9 +34,11 @@ public class OrderController {
             ps.setString(7, CustomerName);
 
             ps.executeUpdate();//test
+            return "{\"status\": \"OK\"}";
 
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to create new item, please see server console for more info.\"}";
         }
 
     }
@@ -43,12 +55,12 @@ public class OrderController {
             while (results.next()) {
                 JSONObject item = new JSONObject();
                 item.put("ReferenceNo",results.getInt(1));
-                item.put("Dateorder",results.getString(2));
+                item.put("DateOrder",results.getString(2));
                 item.put("OrderPrice",results.getDouble(3));
                 item.put("PaymentSuccessful",results.getBoolean(4));
                 item.put("PaymentType",results.getString(5));
-                item.put("DeliveryOrCollection",results.getString(4));
-                item.put("CustomerName",results.getString(5));
+                item.put("DeliveryOrCollection",results.getString(6));
+                item.put("CustomerName",results.getString(7));
                 list.add(item);
             }
             return list.toString();
@@ -60,14 +72,13 @@ public class OrderController {
 
     public static void OrderUpdate(int ReferenceNo, String DateOrder, Double OrderPrice, Boolean PaymentSuccessful, String PaymentType, String DeliveryOrCollection, String CustomerName) {
         try {
-            PreparedStatement ps = Main.db.prepareStatement("UPDATE Orders SET ReferenceNo = ?, DateOrder = ?, OrderPrice = ?, PaymentSuccessful = ?, PaymentType = ?, DeliveryOrCollection = ?, CustomerName = ?");
-            ps.setInt(1, ReferenceNo);
-            ps.setString(2, DateOrder);
-            ps.setDouble(3, OrderPrice);
-            ps.setBoolean(4, PaymentSuccessful);
-            ps.setString(5, PaymentType);
-            ps.setString(6, DeliveryOrCollection);
-            ps.setString(7, CustomerName);
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE Orders SET DateOrder = ?, OrderPrice = ?, PaymentSuccessful = ?, PaymentType = ?, DeliveryOrCollection = ?, CustomerName = ? WHERE ReferenceNo = RefNo");
+            ps.setString(1, DateOrder);
+            ps.setDouble(2, OrderPrice);
+            ps.setBoolean(3, PaymentSuccessful);
+            ps.setString(4, PaymentType);
+            ps.setString(5, DeliveryOrCollection);
+            ps.setString(6, CustomerName);
 
             ps.execute();
         } catch (Exception exception) {
