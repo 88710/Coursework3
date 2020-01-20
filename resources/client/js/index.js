178e7a3f02f89a886f50681e4a59cdc77529d3df
88710 <88710@farnborough.ac.uk>
2020-01-20 22:43:28 +0000

    Last commit 19/01/19

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -4,10 +4,14 @@ function pageLoadCheckout() {
 
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<div style="text-align:left;">'
         + '<h2>My Basket:</h2>'
+
+
         + '<h4>Enter Voucher Below</h4>'
         + '<form>'
         + '<input type="text" name="Voucher">'
@@ -29,39 +33,40 @@ function pageLoadIndex() {
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<img src="/client/img/PizzaBanner.jpg"  alt="Logo"/>'
         + '<h2 style="text-align:right;">Small Medium Large</h2>'
         + '<div style="text-align:Left;">'
         + '<h2>Margerhita</h2>'
-        + '<button>Add Small</button>'
-        + '<button>Add Medium</button>'
-        + '<button>Add Large</button>'
+        + '<button>Small</button>'
+        + '<button>Medium</button>'
+        + '<button>Large</button>'
         + '<h2 style="text-align:right;">8.99 10.99 13.99</h2>'
         + '<div style="text-align:Left;">'
         + '<h2>Pepperoni</h2>'
-        + '<button>Add Small</button>'
-        + '<button>Add Medium</button>'
-        + '<button>Add Large</button>'
+        + '<button>Small</button>'
+        + '<button>Medium</button>'
+        + '<button>Large</button>'
         + '<h2 style="text-align:right;">9.99 12.99 15.99</h2>'
         + '<div style="text-align:Left;">'
         + '<h2>Meat Feast</h2>'
-        + '<button>Add Small</button>'
-        + '<button>Add Medium</button>'
-        + '<button>Add Large</button>'
+        + '<button>Small</button>'
+        + '<button>Medium</button>'
+        + '<button>Large</button>'
         + '<h2 style="text-align:right;">9.99 12.99 15.99</h2>'
         + '<div style="text-align:Left;">'
         + '<h2>Vegi Passion</h2>'
-        + '<button>Add Small</button>'
-        + '<button>Add Medium</button>'
-        + '<button>Add Large</button>'
+        + '<button>Small</button>'
+        + '<button>Medium</button>'
+        + '<button>Large</button>'
         + '<h2 style="text-align:right;">9.99 12.99 15.99</h2>'
         + '<div style="text-align:Left;">'
         + '<h2>Seafood Supreme</h2>'
-        + '<button>Add Small</button>'
-        + '<button>Add Medium</button>'
-        + '<button>Add Large</button>'
+        + '<button>Small</button>'
+        + '<button>Medium</button>'
+        + '<button>Large</button>'
         + '<h2 style="text-align:right;">9.99 12.99 15.99</h2>'
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
@@ -77,6 +82,7 @@ function pageLoadPayment() {
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<div style="text-align:left;">'
@@ -117,6 +123,7 @@ function pageLoadDelivery() {
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<div style="text-align:left;">'
@@ -158,6 +165,8 @@ function pageLoadSuccess() {
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
+
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<h1>Payment Successful</h1>'
@@ -169,4 +178,85 @@ function pageLoadSuccess() {
 
     document.getElementById("testDiv").innerHTML = myHTML;
 
+}
+function pageLoadAdmin() {
+
+    let now = new Date();
+
+    let myHTML = '<div style="text-align:Left;">'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
+        + '<div style="text-align:center;">'
+        + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
+        + '<div style="font-style: italic;">'
+        + 'Generated at ' + now.toLocaleTimeString()
+        + '</div>';
+
+    document.getElementById("testDiv").innerHTML = myHTML;
+
+}
+function pageLoadAdminLogin() {
+
+    let now = new Date();
+
+    let myHTML = '<div style="text-align:Left;">'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
+        + '<div style="text-align:center;">'
+        + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
+        + '<h4>Admin Password</h4>'
+        + '<form>'
+        + '<input type="text" name="AdminPass">'
+        + '</form>'
+        + '<button onclick="window.location.href = \'http://localhost:8081/client/Admin.html\';">Login</button>'
+        + '<div style="font-style: italic;">'
+        + 'Generated at ' + now.toLocaleTimeString()
+        + '</div>';
+
+    document.getElementById("testDiv").innerHTML = myHTML;
+
+}
+
+function editPizza(event,PizzaId,PizzaName,Vegetarian,Vegan,GlutenFree) {
+
+    const id = event.target.getAttribute("data-id");
+
+    if (id === null) {
+
+        document.getElementById("editHeading").innerHTML = 'Add new Pizza:';
+
+        document.getElementById("PizzaId").value = '';
+        document.getElementById("PizzaName").value = '';
+        document.getElementById("Vegetarian").value = '';
+        document.getElementById("Vegan").value = '';
+        document.getElementById("GlutenFree").value = '';
+
+        document.getElementById("listDiv").style.display = 'none';
+        document.getElementById("editDiv").style.display = 'block';
+
+    } else {
+        fetch('/pizza/get/' + id, {method: 'get'}
+        ).then(response => response.json()
+        ).then(fruit => {
+
+            if (fruit.hasOwnProperty('error')) {
+                alert(fruit.error);
+            } else {
+
+                document.getElementById("editHeading").innerHTML = 'Editing ' + fruit.name + ':';
+
+                document.getElementById("PizzaId").value = PizzaId;
+                document.getElementById("PizzaName").value = PizzaName;
+                document.getElementById("Vegetarian").value = Vegetarian;
+                document.getElementById("Vegan").value = Vegan;
+                document.getElementById("GlutenFree").value = GlutenFree;
+
+                document.getElementById("listDiv").style.display = 'none';
+                document.getElementById("editDiv").style.display = 'block';
+
+            }
+        });
+    }
 }
