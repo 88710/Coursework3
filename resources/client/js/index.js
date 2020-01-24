0b639a69b31b77bc845e57aed106da1c0c1d2119
88710 <88710@farnborough.ac.uk>
2020-01-24 00:04:24 +0000

    Last commit 23/01/19

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -1,8 +1,6 @@
 
 function pageLoadCheckout() {
-
     let now = new Date();
-
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
@@ -21,9 +19,7 @@ function pageLoadCheckout() {
         + '<button onclick="window.location.href = \'http://localhost:8081/client/delivery.html\';">Proceed To Checkout</button>'
         + '<div style="font-style: italic;" style="text-align:right;">'
         + 'Generated at ' + now.toLocaleTimeString()
-
     document.getElementById("testDiv").innerHTML = myHTML;
-
 }
 
 function pageLoadIndex() {
@@ -38,7 +34,7 @@ function pageLoadIndex() {
         + '<img src="/client/img/PizzaBanner.jpg"  alt="Logo"/>'
         + '<h2 style="text-align:right;">Small Medium Large</h2>'
         + '<div style="text-align:Left;">'
-        + '<h2>Margerhita</h2>'
+        + '<h2>Margherita</h2>'
         + '<button onclick=alert("Added_to_Basket");>Small</button>'
         + '<button onclick=alert("Added_to_Basket");>Medium</button>'
         + '<button onclick=alert("Added_to_Basket");>Large</button>'
@@ -85,28 +81,23 @@ function pageLoadPayment() {
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<div style="text-align:left;">'
-
         + '<h4>Forename</h4>'
         + '<form>'
         + '<input type="text" name="CustomerName">'
         + '</form>'
-
         + '<h4>16-Digit Card Number</h4>'
         + '<form>'
         + '<input type="text" name="CardNo">'
         + '</form>'
-
         + '<h4>Expiry Date</h4>'
         + '<form>'
         + '<input type="text" name="ExpiryDate">'
         + '</form>'
-
         + '<h4>CCV</h4>'
         + '<form>'
         + '<input type="text" name="CCV">'
         + '</form>'
         + '<div style="text-align:center;">'
-        + '<button onclick=alert("InvalidExpiryDate");>Make Payment</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/success.html\';">Make Payment</button>'
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
@@ -117,9 +108,7 @@ function pageLoadPayment() {
 
 }
 function pageLoadDelivery() {
-
     let now = new Date();
-
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
@@ -127,58 +116,43 @@ function pageLoadDelivery() {
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<div style="text-align:left;">'
-
         + '<h4>House Number</h4>'
         + '<form>'
         + '<input type="text" name="HouseNumber">'
         + '</form>'
-
         + '<h4>Street Name</h4>'
         + '<form>'
         + '<input type="text" name="StreetName">'
         + '</form>'
-
         + '<h4>Town</h4>'
         + '<form>'
         + '<input type="text" name="Town">'
         + '</form>'
-
         + '<h4>Postcode</h4>'
         + '<form>'
         + '<input type="text" name="Postcode">'
         + '</form>'
-
         + '<div style="text-align:center;">'
-        + '<button onclick=alert("InvalidExpiryDate");>Proceed To Payment</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/payment.html\';">Proceed To Payment</button>'
-
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
         + '</div>';
-
     document.getElementById("testDiv").innerHTML = myHTML;
-
 }
 function pageLoadSuccess() {
-
     let now = new Date();
-
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
-
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<h1>Payment Successful</h1>'
         + '<h3>Your order will be with you shortly</h3>'
-
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
         + '</div>';
-
     document.getElementById("testDiv").innerHTML = myHTML;
-
 }
 function pageLoadFail() {
 
@@ -202,9 +176,7 @@ function pageLoadFail() {
 
 }
 function pageLoadAdmin() {
-
     let now = new Date();
-
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
@@ -230,9 +202,7 @@ function pageLoadAdmin() {
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
         + '</div>';
-
     document.getElementById("testDiv").innerHTML = myHTML;
-
 }
 function pageLoadAdminLogin() {
 
@@ -257,14 +227,11 @@ function pageLoadAdminLogin() {
 
 }
 function pageLoadPizzaEdit() {
-
     let now = new Date();
-
     let myHTML = '<div style="text-align:Left;">'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/index.html\';">Home</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/checkout.html\';">Basket</button>'
         + '<button onclick="window.location.href = \'http://localhost:8081/client/AdminLogin.html\';">Admin</button>'
-
         + '<div style="text-align:center;">'
         + '<img src="/client/img/Hawkeye.PNG"  alt="Logo"/>'
         + '<div style="text-align:left;">'
@@ -282,66 +249,49 @@ function pageLoadPizzaEdit() {
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
         + '</div>';
-
     document.getElementById("testDiv").innerHTML = myHTML;
-
 }
 
 function editPizza(event,PizzaId,PizzaName,Vegetarian,Vegan,GlutenFree) {
-
     const id = event.target.getAttribute("data-id");
-
     if (id === null) {
-
         document.getElementById("editHeading").innerHTML = 'Add new Pizza:';
-
         document.getElementById("PizzaId").value = '';
         document.getElementById("PizzaName").value = '';
         document.getElementById("Vegetarian").value = '';
         document.getElementById("Vegan").value = '';
         document.getElementById("GlutenFree").value = '';
-
         document.getElementById("listDiv").style.display = 'none';
         document.getElementById("editDiv").style.display = 'block';
-
     } else {
         fetch('/pizzas/get/' + id, {method: 'get'}
         ).then(response => response.json()
         ).then(fruit => {
-
             if (fruit.hasOwnProperty('error')) {
                 alert(fruit.error);
             } else {
-
                 document.getElementById("editHeading").innerHTML = 'Editing ' + fruit.name + ':';
-
                 document.getElementById("PizzaId").value = PizzaId;
                 document.getElementById("PizzaName").value = PizzaName;
                 document.getElementById("Vegetarian").value = Vegetarian;
                 document.getElementById("Vegan").value = Vegan;
                 document.getElementById("GlutenFree").value = GlutenFree;
-
                 document.getElementById("listDiv").style.display = 'none';
                 document.getElementById("editDiv").style.display = 'block';
-
             }
         });
     }
 }
 function saveEditPizza(event){
-
     event.preventDefault();
-
     if (document.getElementById("PizzaName").value.trim() === '') {
         alert("Please provide a pizza name.");
         return;
     }
-
     if (document.getElementById("Vegetarian").value.trim() === '') {
         alert("Is this pizza vegetarian?.");
         return;
     }
-
     if (document.getElementById("Vegan").value.trim() === '') {
         alert("Is this pizza Vegan?.");
         return;
@@ -353,18 +303,15 @@ function saveEditPizza(event){
     const id = document.getElementById("PizzaId").value;
     const form = document.getElementById("fruitForm");
     const formData = new FormData(form);
-
     let apiPath = '';
     if (id === '') {
         apiPath = '/pizzas/new';
     } else {
         apiPath = '/pizzas/update';
     }
-
     fetch(apiPath, {method: 'post', body: formData}
     ).then(response => response.json()
     ).then(responseData => {
-
         if (responseData.hasOwnProperty('error')) {
             alert(responseData.error);
         } else {
@@ -383,19 +330,14 @@ function cancelEditPizza(event) {
 
 }
 function deletePizza(event) {
-
     const ok = confirm("Are you sure?");
-
     if (ok === true) {
-
         let id = event.target.getAttribute("data-id");
         let formData = new FormData();
         formData.append("id", id);
-
         fetch('/pizzas/delete', {method: 'post', body: formData}
         ).then(response => response.json()
         ).then(responseData => {
-
                 if (responseData.hasOwnProperty('error')) {
                     alert(responseData.error);
                 } else {
